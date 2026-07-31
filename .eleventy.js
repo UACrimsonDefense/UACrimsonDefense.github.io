f5d6bd01c4d2306e89c4949e3a1d4ce170dda46a
Samuel Bozeman <sbozeman@proton.me>
2026-07-31 16:37:02 -0500

    Completed the construction of the footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/base.css");
     eleventyConfig.addPassthroughCopy("css/nav.css");
+    eleventyConfig.addPassthroughCopy("css/footer.css");
     eleventyConfig.addPassthroughCopy("img/CrimsonDefenseLogoTransparent.svg");
     eleventyConfig.addPassthroughCopy("js/nav.js");
 };
