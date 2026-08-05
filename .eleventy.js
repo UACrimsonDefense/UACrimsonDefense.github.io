1eb83aae78246aae41b1f084dc8c22b68a4b4303
Samuel Bozeman <sbozeman@proton.me>
2026-08-04 19:57:42 -0500

    Added the UACTF and CDCTF page, fixed licensing in package.json

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,12 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("css/index.css");
     eleventyConfig.addPassthroughCopy("css/club-leadership.css");
     eleventyConfig.addPassthroughCopy("css/meetings.css");
+    eleventyConfig.addPassthroughCopy("css/cdctf.css");
+    eleventyConfig.addPassthroughCopy("css/uactf.css");
+    eleventyConfig.addPassthroughCopy("css/scoreboard.css");
     eleventyConfig.addPassthroughCopy("img/transparentCDLogo.png");
     eleventyConfig.addPassthroughCopy("img/ClubPainting.jpg");
+    eleventyConfig.addPassthroughCopy("img/CDCTF_logo_fin.svg");
+    eleventyConfig.addPassthroughCopy("img/UACTF_double_logo.png");
     eleventyConfig.addPassthroughCopy("js/nav.js");
 };
