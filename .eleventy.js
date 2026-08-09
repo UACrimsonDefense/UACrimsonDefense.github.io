245c1bb39ca256ef8c996b55ad923be0a3209e6f
Samuel Bozeman <sbozeman@proton.me>
2026-08-08 20:16:41 -0500

    Added .cache to gitingore, added some images, made the competition team and about page, and added eleventy-img for image optimization.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,21 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function(eleventyConfig) {
+    eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+        formats: ["webp", "auto"],
+        widths: ["auto"],
+        defaultAttributes: {
+            loading: "lazy",
+            decoding: "async",
+        },
+    });
+
     eleventyConfig.addPassthroughCopy("css/base.css");
     eleventyConfig.addPassthroughCopy("css/nav.css");
     eleventyConfig.addPassthroughCopy("css/footer.css");
     eleventyConfig.addPassthroughCopy("css/index.css");
+    eleventyConfig.addPassthroughCopy("css/about.css");
+    eleventyConfig.addPassthroughCopy("css/competition-team.css");
     eleventyConfig.addPassthroughCopy("css/club-leadership.css");
     eleventyConfig.addPassthroughCopy("css/meetings.css");
     eleventyConfig.addPassthroughCopy("css/cdctf.css");
@@ -13,5 +26,7 @@ export default function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img/ClubPainting.jpg");
     eleventyConfig.addPassthroughCopy("img/CDCTF_logo_fin.svg");
     eleventyConfig.addPassthroughCopy("img/UACTF_double_logo.png");
+    eleventyConfig.addPassthroughCopy("img/Crimson-Defense-X.png");
+    eleventyConfig.addPassthroughCopy("img/ua_ccdcteam_2026.jpeg");
     eleventyConfig.addPassthroughCopy("js/nav.js");
 };
